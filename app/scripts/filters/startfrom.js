3166494203d8a6c35c6f6c55ae4e569ac72c59fe
Jorge Bayona <jorge.bayona@unosquare.com>
2016-03-10 18:10:35 -0600

    Adding comments to pagination feature

--- app/scripts/filters/startfrom.js
+++ app/scripts/filters/startfrom.js
@@ -15,6 +15,7 @@ angular.module('tendProgramApp')
     		return;
     	}
       start = +start;
-      return input.slice(start);
+      return input.slice(start); //Cortamos el array que nos va a regresar para mostrarlo
+      //En este caso vamos a cortar los elementos que vengan, por ejemplo si son 20, vamos a cortar los primeros 20 o lo que vengan
     };
   });
